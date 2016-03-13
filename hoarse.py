7fa14853ff67aa1a294e0c85d076b6ac2753ff6b
SDG <sdg-git@jlbl.net>
2016-03-13 13:10:57 +0100

    First draft of the competitor management screen

--- hoarse.py
+++ hoarse.py
@@ -3,6 +3,8 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+
 
 from kivy.properties import StringProperty,  ObjectProperty
 
@@ -17,7 +19,20 @@ class SubstyleKoreanMenu(BoxLayout):
     pass
 
 class CompetitorsManagementMenu(BoxLayout):
-    pass
+    counter = 0
+    def add_competitor(self, name):
+        self.counter += 1
+        self.ids['competitors_list'].add_widget(CompetitorLine(name=name))
+
+class CompetitorLine(BoxLayout):
+    def __init__(self, name):
+        super().__init__()
+        self.ids["name"].text = name
+
+class CompetitorTextInput(TextInput):
+
+    def on_text_validate(self):
+        self.parent.add_competitor(name=self.text)
 
 class HoarseApp(App):
     def build(self):
