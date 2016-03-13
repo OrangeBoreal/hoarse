0c5fa3b164cb0063088cc0a77d4bc668ace25413
SDG <sdg-git@jlbl.net>
2016-03-13 11:22:16 +0100

    Added Screen manager and 2 screens (dirty but IT WORKS !)

--- hoarse.py
+++ hoarse.py
@@ -1,10 +1,27 @@
 import kivy
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
 
+from kivy.properties import StringProperty,  ObjectProperty
 
-class TalaApp(App):
+class StyleButton(Button):
+    name = StringProperty()
+
+class StyleMenu(BoxLayout):
     pass
+
+class SubstyleKoreanMenu(BoxLayout):
+    pass
+
+class HoarseApp(App):
+    def build(self):
+        return HoarseMain()
+
+class HoarseMain(BoxLayout):
+    manager = ObjectProperty(None)
 
 
 if __name__ == "__main__":
-    TalaApp().run()
+    HoarseApp().run()
