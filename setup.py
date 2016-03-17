9cee4a32a589a728ac666c3bcb9c30310c776803
Joachim Jablon <ewjoachim@gmail.com>
2016-03-17 21:23:19 +0100

    Various reorganizations

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "hoarse",
-    version = "0.1",
-    packages = find_packages(),
+    name="hoarse",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "kivy==1.9.1",
+        "Cython==0.23",
+    ]
 )
